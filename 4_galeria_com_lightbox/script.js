f5d5b1b85b2097edfc39481c38419e5233bc99ae
Rayane Galdino <rayaneprado2005@gmail.com>
2024-10-19 20:58:28 -0300

    javascript and html adjust

--- 4_galeria_com_lightbox/script.js
+++ 4_galeria_com_lightbox/script.js
@@ -1,18 +1,18 @@
-const galleryItems = document.querySelectorAll(".gallery.item");
-const lightbox = document.querySelectorAll(".lightbox");
-const lightboxImage = document.querySelectorAll(".lightbox-image");
-const lightboxClose = document.querySelectorAll(".lightbox.close");
+const galleryItems = document.querySelectorAll(".gallery-item");
+const lightbox = document.querySelector(".lightbox");
+const lightboxImage = document.querySelector(".lightbox-image");
+const lightboxClose = document.querySelector(".lightbox-close");
 
 galleryItems.forEach((item) => {
     item.addEventListener("click", () => {
         const imageUrl = item
-        .querySelector(".gellery-image")
-        .getAttribute("data-src");
+        .querySelector(".gallery-image")
+        .getAttribute("src");
         lightboxImage.setAttribute("src", imageUrl);
         lightbox.style.display = "flex";
     });    
-})
+});
 
 lightboxClose.addEventListener("click", () => {
     lightbox.style.display = "none";
-})
+});

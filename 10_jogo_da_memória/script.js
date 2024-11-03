85cb32bd7ea06c47d992c8dcc25f21dc5b7f1270
Rayane Galdino <rayaneprado2005@gmail.com>
2024-11-02 22:42:34 -0300

    Introduction of function to generate image pairs for cards

---
+++ 10_jogo_da_memória/script.js
@@ -0,0 +1,11 @@
+const cards = [1, 1, 2, 2, 3, 3, 4, 4];
+
+async function  generateImageParis() {
+    const imagePairs = {};
+    for (let i = 0; i < cards.length; i++) {
+        if (!imagePairs[cards[i]]) {
+            const id = Math.floor(Math.random() * 1000) + 1;
+            const url = `https://pic`
+        }
+    } 
+}

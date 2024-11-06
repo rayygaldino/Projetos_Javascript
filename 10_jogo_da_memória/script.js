60588ef9475491bd3a67fecf25ab783582094441
Rayane Galdino <rayaneprado2005@gmail.com>
2024-11-05 23:23:52 -0300

    Creating cards - JS functions

--- 10_jogo_da_memória/script.js
+++ 10_jogo_da_memória/script.js
@@ -5,7 +5,7 @@ async function  generateImageParis() {
     for (let i = 0; i < cards.length; i++) {
         if (!imagePairs[cards[i]]) {
             const id = Math.floor(Math.random() * 1000) + 1;
-            const url = `https://picsum,photos/id/${id}/300/400`;
+            const url = `https://picsum.photos/id/${id}/300/400`;
             imagePairs[cards[i]] = [url,url];
         }
     } 
@@ -20,6 +20,28 @@ function shuffleCards(cards) {
 async function createCards() {
     const imagePairs = await generateImageParis();
     shuffleCards(cards);
+    const cardList = document.querySelector(".container")
+    for (let i = 0; i < cards.length; i++) {
+        const card = document.createElement("div");
+        const cardBack = document.createElement("div");
+        const cardFront = document.createElement("div");
+
+        card.classList.add("card");
+        cardBack.classList.add("back");
+        cardFront.classList.add("front");
+
+         cardBack.style.backgroundImage = `url('img/card-back.png')`;
+
+        const cardNumber = cards[i];
+        const cardImage = imagePairs[cardNumber].pop();
+
+        cardFront.style.backgroundImage = `url(${cardImage})`;
+
+        card.setAttribute("data-card", cardNumber);
+        card.append(cardBack);
+        card.appendChild(cardFront);
+        cardList.appendChild(card);
+    }
 
     console.log(cards);
 }
